e86dc1425600cdb662c94cbd4663ad540aad781b
jidckii <jidckii@gmail.com>
2016-11-04 19:49:08 +0500

    убрал лишнее

--- bin/weburg-film-converter.sh
+++ bin/weburg-film-converter.sh
@@ -5,12 +5,8 @@ source_path=/home/torrent/source-video-tmp/
 end_path=/home/torrent/end-video-tmp/
 log_dir=/home/torrent/logs/
 
-video_search="Обнаружен материал объемом"
-wait_video="Ожидается пауза при копировании в папку с очередью"
-
 while true; do
   tmp_video_size1=`du -s $queue_path | awk '{print $1}'`
-  # echo -e '\n' "\e[0;32m $tmp_video_size1 \e[0m" '\n'
   sleep 2
   while [ "$tmp_video_size1" -gt 1000 ]; do   # Проверям размер раталога
     tmp_video_size1=`du -s $queue_path | awk '{print $1}'`
@@ -22,7 +18,6 @@ while true; do
       continue
     fi
 
-    # date_time=`date +%H:%M_%d-%m-%Y`
     next_file=`ls -t -r -1 $queue_path | sed -n -e 1p`
     mv $queue_path$next_file $source_path       # перемещаем из очереди в рабочий каталог
     end_file_name=`ls -1 $source_path`
@@ -46,20 +41,23 @@ while true; do
       ps_status=`ps -e | grep ffmpeg | wc -l`
     done
 
-    # mv $trans_source_path$end_file_n2.mp4 $end_path
-
-    full_time=`ffprobe $end_path$end_file_n2.mp4 2>&1 | grep Duration | awk '{print $2}' | sed s'/,//g'`
-    echo -e $full_time
-
-
     mkdir $end_path$end_file_n2\_chunks
 
     ffmpeg \
           -i $end_path$end_file_n2.mp4 -map 0 -c copy -segment_time 3 \
           -segment_list $end_path$end_file_n2/$end_file_n2.m3u8 -f segment \
           $end_path$end_file_n2/$end_file_n2\_%08d.ts > $log_dir$end_file_n2\_seg.log 2>&1 &
+    
+    sleep 1
+    ps_status=`ps -e | grep ffmpeg | wc -l`
+    while [ "$ps_status" -gt "0" ]; do
+      sleep 2
+      ps_status=`ps -e | grep ffmpeg | wc -l`
+    done
 
-    exit 0
+    
+
+
     break
   done
 done
