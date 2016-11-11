6d8be00727706225aaaa6277ada7a12c1b2c0ed6
leoderoko <leoderoko@gmail.com>
2016-11-11 12:29:41 +0500

    Use functions

--- petr.sh
+++ petr.sh
@@ -3,7 +3,6 @@
 source "functions.sh"
 
 user=emedvedev
-api_url="http://api.peskar.paradev.ru"
 
 queue_path=/home/$user/queue/
 source_path=/home/$user/source/
@@ -13,52 +12,39 @@ log_path=/home/$user/logs/
 paradev_path=/home/user/films/
 state_failed=0
 
-date_time_up () {
-  date_time=`date +%Y-%m-%dT%T%Z`
-}
-
-while true;do
-
-  date_time_up
-  job_id=`curl -s $api_url/ping/ | jq '.id' | tr -d \"`
-
+while true; do
+  job_id=$(job_ping)
   if [ $job_id == "null" ]; then
     exit 0
   fi
 
-  job_state=`curl -s $api_url/job/$job_id/ | jq '.state' | tr -d \"`
-
+  job_state=$(job_get_state $job_id)
   if [ $job_state == "pending" ]; then
     let "state_failed=state_failed += 1"
     if [ "$state_failed" -gt 4 ]; then
       echo -e "job_state not change to requested" > $log_path$job_id.log
-      curl -X PUT -d '{"state": "failed", "log": "job_state not change to requested"}' $api_url/job/$job_id/ > /dev/null 2>&1
+      job_set_failed $job_id "job_state not change to requested"
     fi
     continue
   elif [ $job_state == "requested" ]; then
-    date_time_up
-    curl -X PUT -d '{"state": "working", "log": "'$date_time' add to working..."}' $api_url/job/$job_id/ > /dev/null 2>&1
+    job_set_working $job_id "Add to working..."
   fi
 
-  job_download_url=`curl -s $api_url/job/$job_id/ | jq '.download_url' | tr -d \"`
-  # job_name=`curl -s $api_url/job/$job_id/ | jq '.name' | tr -d \"`
+  job_download_url=$(job_get_url $job_id)
 
   if [ $job_download_url == "null" ]; then
-    date_time_up
-    curl -X PUT -d '{"state": "failed", "log": "'$date_time' download_url not found"}' $api_url/job/$job_id/ > /dev/null 2>&1
+    job_set_failed $job_id "URL not found"
     exit 0
   fi
 
   file_name=`echo $job_download_url | awk -F/ '{print $NF}'`
   mkdir $queue_path$job_id
   curl -s -o $queue_path$job_id/$file_name $job_download_url & pid_curl=$!
-  date_time_up
-  curl -X PUT -d '{"state": "working", "log": "'$date_time' Starting download..."}' $api_url/job/$job_id/ > /dev/null 2>&1
+  job_log $job_id "Starting download..."
 
   wait $pid_curl
 
-  date_time_up
-  curl -X PUT -d '{"state": "working", "log": "'$date_time' Downloaded, sending to transcoder..."}' $api_url/job/$job_id/ > /dev/null 2>&1
+  job_log $job_id "Downloaded, sending to transcoder..."
 
   end_name=`echo $file_name | awk -F. '{print $1}'`
 
@@ -77,14 +63,13 @@ while true;do
         -i $queue_path$job_id/$file_name -c:v libx264 -preset veryfast -g 25 -keyint_min 4\
         -c:a aac -f mp4 $source_path$job_id/$end_name.mp4 > $log_path$job_id/$end_name.log 2>&1 & pid_ffmpeg=$!
 
-  date_time_up
-  curl -X PUT -d '{"state": "working", "log": "'$date_time' Starting transcoding..."}' $api_url/job/$job_id/ > /dev/null 2>&1
+  job_log $job_id "Starting transcoding..."
 
   wait $pid_ffmpeg
 
   file_size=`wc -c $source_path$job_id/$end_name.mp4 | awk '{print $1}'`
   if [ $file_size -lt 1 ]; then
-    curl -X PUT -d '{"state": "failed", "log": "'$date_time' Transcoding error "}' $api_url/job/$job_id/ > /dev/null 2>&1
+    job_set_failed "Transcoding error"
 
     tar -c -f $end_path$job_id/$end_name.tar $log_path$job_id/$end_name.log > /dev/null 2>&1
     rsync -e='ssh -p 3389' -r $end_path$job_id/$end_name.tar user@paradev.ru:$paradev_path
@@ -93,8 +78,7 @@ while true;do
     exit 0
   fi
 
-  date_time_up
-  curl -X PUT -d '{"state": "working", "log": "'$date_time' Transcoding finished, sending to copying..."}' $api_url/job/$job_id/ > /dev/null 2>&1
+  job_log "Transcoding finished, sending to copying.."
 
   ffmpeg \
         -i $source_path$job_id/$end_name.mp4 -map 0 -c copy -segment_time 3 \
@@ -107,14 +91,12 @@ while true;do
   cp $end_path$job_id/logs_$end_name.tar.gz $end_path$job_id/
   tar -c -f $end_path$job_id/$end_name.tar $end_path$job_id/$end_name/* > /dev/null 2>&1
 
-  date_time_up
-  curl -X PUT -d '{"state": "working", "log": "'$date_time' Starting copying..."}' $api_url/job/$job_id/ > /dev/null 2>&1
+  job_log "Starting copying..."
 
   rsync -e='ssh -p 3389' -r $end_path$job_id/$end_name.tar user@paradev.ru:$paradev_path
 
-  date_time_up
-  curl -X PUT -d '{"state": "working", "log": "'$date_time' Copying finished."}' $api_url/job/$job_id/ > /dev/null 2>&1
-  curl -X PUT -d '{"state": "finished", "log": "'$date_time' Done"}' $api_url/job/$job_id/ > /dev/null 2>&1
+  job_log "Copying finished"
+  job_set_finished "Done"
 
   rm -r -f queue_path$job_id && rm -r -f $source_path$job_id && rm -r -f $end_path$job_id > /dev/null 2>&1
   break
