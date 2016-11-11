5d9f419e34a91224683e2ba0fc70678ca075be3b
leoderoko <leoderoko@gmail.com>
2016-11-11 16:15:46 +0500

    Fix path

--- petr.sh
+++ petr.sh
@@ -12,29 +12,14 @@ end_path=$base_path/end/
 finish_path=$base_path/finish/
 log_path=$base_path/logs/
 
-pre_work_hook() {
-  local JOB_ID="$1"
-
-  mkdir -p $queue_path/$JOB_ID \
-    $source_path/$JOB_ID \
-    $end_path/$JOB_ID \
-    $finish_path/$JOB_ID \
-    $log_path/$JOB_ID \
-}
-
-post_work_hook() {
-  local JOB_ID="$1"
-
-  rm -rf $queue_path/$JOB_ID \
-    $source_path/$JOB_ID \
-    $end_path/$JOB_ID \
-    $finish_path/$JOB_ID \
-    $log_path/$JOB_ID \
-    > /dev/null 2>&1
-}
-
 worker() {
   local JOB_ID="$1"
+
+  local QUEUE_PATH="$queue_path/$JOB_ID"
+  local SOURCE_PATH="$source_path/$JOB_ID"
+  local END_PATH="$end_path/$JOB_ID"
+  local FINISH_PATH="$finish_path/$JOB_ID"
+  local LOG_PATH="$log_path/$JOB_ID"
 
   job_set_working $JOB_ID "Add to working..."
   job_download_url=$(job_get_url $JOB_ID)
@@ -43,13 +28,13 @@ worker() {
     return
   fi
 
-  pre_work_hook $JOB_ID
+  mkdir -p $QUEUE_PATH $SOURCE_PATH $END_PATH $FINISH_PATH $LOG_PATH
 
   file_name=$(echo $job_download_url | awk -F/ '{print $NF}')
   end_name=$(echo $file_name | awk -F. '{print $1}')
 
   job_log $JOB_ID "Starting downloading..."
-  curl -s -o $queue_path/$JOB_ID/$file_name $job_download_url & pid_curl=$!
+  curl -s -o $QUEUE_PATH/$file_name $job_download_url & pid_curl=$!
   wait $pid_curl
   job_log $JOB_ID "Downloading finished"
 
@@ -64,44 +49,45 @@ worker() {
 
   job_log $JOB_ID "Starting transcoding..."
   ffmpeg \
-    -i $queue_path/$JOB_ID/$file_name -c:v libx264 -preset veryfast -g 25 -keyint_min 4\
-    -c:a aac -f mp4 $source_path/$JOB_ID/$end_name.mp4 > $log_path/$JOB_ID/$end_name.log 2>&1 & pid_ffmpeg=$!
+    -i $QUEUE_PATH/$file_name -c:v libx264 -preset veryfast \
+    -g 25 -keyint_min 4 -c:a aac -f mp4 \
+    $SOURCE_PATH/$end_name.mp4 > $LOG_PATH/$end_name.log 2>&1 & pid_ffmpeg=$!
   wait $pid_ffmpeg
 
-  file_size=$(wc -c $source_path/$JOB_ID/$end_name.mp4 | awk '{print $1}')
+  file_size=$(wc -c $SOURCE_PATH/$end_name.mp4 | awk '{print $1}')
   if [ $file_size -lt 1 ]; then
     job_set_failed $JOB_ID "Transcoding error"
-    tar -zcf $end_path/$JOB_ID/logs_$end_name.tar.gz $log_path/$JOB_ID/* > /dev/null 2>&1
+    tar -zcf $END_PATH/logs_$end_name.tar.gz $LOG_PATH/* > /dev/null 2>&1
     rsync \
       -e "\"$PESKAR_SYNC_OPTIONS\"" \
-      -r $end_path/$JOB_ID/logs_$end_name.tar.gz \
+      -r $END_PATH/logs_$end_name.tar.gz \
       $PESKAR_SYNC_TARGET:$PESKAR_SYNC_PATH
-    post_work_hook $JOB_ID
+    rm -rf $QUEUE_PATH $SOURCE_PATH $END_PATH $FINISH_PATH $LOG_PATH
     return
   fi
   job_log $JOB_ID "Transcoding finished"
 
   job_log $JOB_ID "Starting segmenting..."
   ffmpeg \
-    -i $source_path/$JOB_ID/$end_name.mp4 -map 0 -c copy -segment_time 3 \
-    -segment_list $end_path/$JOB_ID/$end_name.m3u8 -f segment \
-    $end_path/$JOB_ID/$end_name\_%08d.ts > $log_path/$JOB_ID/$end_name\_seg.log 2>&1 & pid_ffmpeg=$!
+    -i $SOURCE_PATH/$end_name.mp4 -map 0 -c copy -segment_time 3 \
+    -segment_list $END_PATH/$end_name.m3u8 -f segment \
+    $END_PATH/$end_name\_%08d.ts > $LOG_PATH/$end_name\_seg.log 2>&1 & pid_ffmpeg=$!
   wait $pid_ffmpeg
   job_log $JOB_ID "Segmenting finished"
 
   job_log $JOB_ID "Creating tarball..."
-  tar -zcf $end_path/$JOB_ID/logs_$end_name.tar.gz $log_path/$JOB_ID/* > /dev/null 2>&1
-  tar -cf $finish_path/$JOB_ID/$end_name.tar $end_path/$JOB_ID/* > /dev/null 2>&1
+  tar -zcf $END_PATH/logs_$end_name.tar.gz $LOG_PATH/* > /dev/null 2>&1
+  tar -cf $FINISH_PATH/$end_name.tar $END_PATH/* > /dev/null 2>&1
   job_log $JOB_ID "Creating finished"
 
   job_log $JOB_ID "Starting copying to remote server..."
   rsync \
     -e "\"$PESKAR_SYNC_OPTIONS\"" \
-    -r $finish_path/$JOB_ID/$end_name.tar \
+    -r $FINISH_PATH/$end_name.tar \
     $PESKAR_SYNC_TARGET:$PESKAR_SYNC_PATH
   job_log $JOB_ID "Copying finished"
 
-  post_work_hook $JOB_ID
+  rm -rf $QUEUE_PATH $SOURCE_PATH $END_PATH $FINISH_PATH $LOG_PATH
 
   job_set_finished $JOB_ID "Done"
 }
