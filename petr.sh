bcb7242264c91dafaf35ccf55bd4e49d24688be0
leoderoko <leoderoko@gmail.com>
2016-11-11 15:43:01 +0500

    Init

--- petr.sh
+++ petr.sh
@@ -12,110 +12,112 @@ end_path=$base_path/end/
 finish_path=$base_path/finish/
 log_path=$base_path/logs/
 
-main() {
-  state_failed=0
+pre_work_hook() {
+  local JOB_ID="$1"
 
+  mkdir -p $queue_path$JOB_ID/
+  mkdir -p $source_path$JOB_ID/
+  mkdir -p $end_path$JOB_ID/
+  mkdir -p $finish_path$JOB_ID/
+  mkdir -p $log_path$JOB_ID/
+}
+
+post_work_hook() {
+  local JOB_ID="$1"
+
+  rm -rf $queue_path$JOB_ID > /dev/null 2>&1
+  rm -rf $source_path$JOB_ID > /dev/null 2>&1
+  rm -rf $end_path$JOB_ID > /dev/null 2>&1
+  rm -rf $finish_path$JOB_ID > /dev/null 2>&1
+  rm -rf $log_path$JOB_ID > /dev/null 2>&1
+}
+
+worker() {
+  local JOB_ID="$1"
+
+  job_set_working $JOB_ID "Add to working..."
+
+  job_download_url=$(job_get_url $JOB_ID)
+
+  if [ $job_download_url == "null" ]; then
+    job_set_failed $JOB_ID "URL not found"
+    return
+  fi
+
+  file_name=$(echo $job_download_url | awk -F/ '{print $NF}')
+  end_name=$(echo $file_name | awk -F. '{print $1}')
+
+  pre_work_hook $JOB_ID
+
+  job_log $JOB_ID "Starting downloading..."
+  curl -s -o $queue_path$JOB_ID/$file_name $job_download_url & pid_curl=$!
+  wait $pid_curl
+  job_log $JOB_ID "Downloading finished"
+
+  job_log $JOB_ID "Ensure FFmpeg is not running"
+  ps_status=$(ps -e | grep ffmpeg | wc -l)
+  while [ "$ps_status" -gt "0" ]; do
+    job_log $JOB_ID "FFmpeg is running, sleeping..."
+    sleep 30
+    ps_status=$(ps -e | grep ffmpeg | wc -l)
+  done
+  job_log $JOB_ID "FFmpeg is available"
+
+  job_log $JOB_ID "Starting transcoding..."
+  ffmpeg \
+    -i $queue_path$JOB_ID/$file_name -c:v libx264 -preset veryfast -g 25 -keyint_min 4\
+    -c:a aac -f mp4 $source_path$JOB_ID/$end_name.mp4 > $log_path$JOB_ID/$end_name.log 2>&1 & pid_ffmpeg=$!
+  wait $pid_ffmpeg
+
+  file_size=$(wc -c $source_path$JOB_ID/$end_name.mp4 | awk '{print $1}')
+  if [ $file_size -lt 1 ]; then
+    job_set_failed $JOB_ID "Transcoding error"
+    tar -c -f $end_path$JOB_ID/$end_name.tar $log_path$JOB_ID/$end_name.log > /dev/null 2>&1
+    rsync \
+      -e="ssh -p $PESKAR_STORE_PORT" \
+      -r $end_path$JOB_ID/$end_name.tar \
+      $PESKAR_STORE_USER@$PESKAR_STORE_HOST:$PESKAR_STORE_PATH
+    post_work_hook $JOB_ID
+    return
+  fi
+  job_log $JOB_ID "Transcoding finished"
+
+  job_log $JOB_ID "Starting segmenting..."
+  ffmpeg \
+    -i $source_path$JOB_ID/$end_name.mp4 -map 0 -c copy -segment_time 3 \
+    -segment_list $end_path$JOB_ID/$end_name.m3u8 -f segment \
+    $end_path$JOB_ID/$end_name\_%08d.ts > $log_path$JOB_ID/$end_name\_seg.log 2>&1 & pid_ffmpeg=$!
+  wait $pid_ffmpeg
+  job_log $JOB_ID "Segmenting finished"
+
+  job_log $JOB_ID "Creating tarball..."
+  tar -zcf $end_path$JOB_ID/logs_$end_name.tar.gz $log_path$JOB_ID/* > /dev/null 2>&1
+  tar -cf $finish_path$JOB_ID/$end_name.tar $end_path$JOB_ID/* > /dev/null 2>&1
+  job_log $JOB_ID "Creating finished"
+
+  job_log $JOB_ID "Starting copying to remote server..."
+  rsync \
+    -e="ssh -p $PESKAR_STORE_PORT" \
+    -r $finish_path$JOB_ID/$end_name.tar \
+    $PESKAR_STORE_USER@$PESKAR_STORE_HOST:$PESKAR_STORE_PATH
+  job_log $JOB_ID "Copying finished"
+
+  post_work_hook $JOB_ID
+
+  job_set_finished $JOB_ID "Done"
+}
+
+main() {
   while true; do
     job_id=$(job_ping)
     if [ $job_id == "null" ]; then
-      exit 0
-    fi
-
-    job_state=$(job_get_state $job_id)
-    if [ $job_state == "pending" ]; then
-      let "state_failed=state_failed += 1"
-      if [ "$state_failed" -gt 4 ]; then
-        echo -e "job_state not change to requested" > $log_path$job_id.log
-        job_set_failed $job_id "job_state not change to requested"
-      fi
+      sleep 5m
       continue
-    elif [ $job_state == "requested" ]; then
-      job_set_working $job_id "Add to working..."
     fi
 
-    job_download_url=$(job_get_url $job_id)
-
-    if [ $job_download_url == "null" ]; then
-      job_set_failed $job_id "URL not found"
-      exit 0
-    fi
-
-    file_name=$(echo $job_download_url | awk -F/ '{print $NF}')
-    end_name=$(echo $file_name | awk -F. '{print $1}')
-
-    mkdir -p $queue_path$job_id
-    mkdir -p $source_path$job_id/
-    mkdir -p $end_path$job_id/
-    mkdir -p $finish_path$job_id/
-    mkdir -p $log_path$job_id/
-
-    job_log $job_id "Starting downloading..."
-    curl -s -o $queue_path$job_id/$file_name $job_download_url & pid_curl=$!
-    wait $pid_curl
-    job_log $job_id "Downloading finished"
-
-    job_log $job_id "Ensure FFmpeg is not running"
-    sleep 1
-    ps_status=$(ps -e | grep ffmpeg | wc -l)
-    while [ "$ps_status" -gt "0" ]; do
-      job_log $job_id "FFmpeg is running, sleeping..."
-      sleep 30
-      ps_status=$(ps -e | grep ffmpeg | wc -l)
-    done
-    job_log $job_id "FFmpeg is available"
-
-    job_log $job_id "Starting transcoding..."
-    ffmpeg \
-      -i $queue_path$job_id/$file_name -c:v libx264 -preset veryfast -g 25 -keyint_min 4\
-      -c:a aac -f mp4 $source_path$job_id/$end_name.mp4 > $log_path$job_id/$end_name.log 2>&1 & pid_ffmpeg=$!
-    wait $pid_ffmpeg
-
-    file_size=$(wc -c $source_path$job_id/$end_name.mp4 | awk '{print $1}')
-    if [ $file_size -lt 1 ]; then
-      job_set_failed $job_id "Transcoding error"
-      tar -c -f $end_path$job_id/$end_name.tar $log_path$job_id/$end_name.log > /dev/null 2>&1
-      rsync \
-        -e="ssh -p $PESKAR_STORE_PORT" \
-        -r $end_path$job_id/$end_name.tar \
-        $PESKAR_STORE_USER@$PESKAR_STORE_HOST:$PESKAR_STORE_PATH
-
-      rm -rf $queue_path$job_id > /dev/null 2>&1
-      rm -rf $source_path$job_id > /dev/null 2>&1
-      rm -rf $end_path$job_id > /dev/null 2>&1
-
-      exit 0
-    fi
-    job_log $job_id "Transcoding finished"
-
-    job_log $job_id "Starting segmenting..."
-    ffmpeg \
-      -i $source_path$job_id/$end_name.mp4 -map 0 -c copy -segment_time 3 \
-      -segment_list $end_path$job_id/$end_name.m3u8 -f segment \
-      $end_path$job_id/$end_name\_%08d.ts > $log_path$job_id/$end_name\_seg.log 2>&1 & pid_ffmpeg=$!
-    wait $pid_ffmpeg
-    job_log $job_id "Segmenting finished"
-
-    job_log $job_id "Creating tarball..."
-    tar -zcf $end_path$job_id/logs_$end_name.tar.gz $log_path$job_id/* > /dev/null 2>&1
-    tar -cf $finish_path$job_id/$end_name.tar $end_path$job_id/* > /dev/null 2>&1
-    job_log $job_id "Creating finished"
-
-    job_log $job_id "Starting copying to remote server..."
-    rsync \
-      -e="ssh -p $PESKAR_STORE_PORT" \
-      -r $finish_path$job_id/$end_name.tar \
-      $PESKAR_STORE_USER@$PESKAR_STORE_HOST:$PESKAR_STORE_PATH
-    job_log $job_id "Copying finished"
-
-    rm -rf $queue_path$job_id > /dev/null 2>&1
-    rm -rf $source_path$job_id > /dev/null 2>&1
-    rm -rf $end_path$job_id > /dev/null 2>&1
-    rm -rf $finish_path$job_id > /dev/null 2>&1
-  	rm -rf $log_path$job_id > /dev/null 2>&1
-
-    job_set_finished $job_id "Done"
-    break
+    worker $job_id &
+    sleep 1m
   done
 }
 
-main "$@"
+main_test "$@"
