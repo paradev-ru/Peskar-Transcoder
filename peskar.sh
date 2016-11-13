1ab328566523e67b63f1ab9b9b58dbb8d854c2bc
leoderoko <leoderoko@gmail.com>
2016-11-13 15:36:46 +0500

    Rename and fix codestyle

--- peskar.sh
+++ peskar.sh
@@ -30,18 +30,17 @@ main() {
     log_fail "Unable create ${PESKAR_PETR_HOME_PATH}/jobs/ directory"
   fi
   while true; do
-    can_work=$(is_work_time)
-    if [ $can_work != "true" ]; then
+    if [[ "$(is_work_time)" != "true" ]]; then
       sleep 10m
       continue
     fi
     job_id=$(job_ping)
-    if [ $job_id == "null" ]; then
+    if [[ "${job_id}" == "null" ]]; then
       sleep 30s
       continue
     fi
-    log_info "Starting a job '$job_id'..."
-    worker $job_id &
+    log_info "Starting a job ${job_id}..."
+    worker "${job_id}" &
     sleep 1m
   done
 }
