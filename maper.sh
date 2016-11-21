a23f66abf6bca7f815c8a812c24aa620fc43369f
leoderoko <leoderoko@gmail.com>
2016-11-21 23:44:14 +0500

    Fix message

--- maper.sh
+++ maper.sh
@@ -17,24 +17,24 @@ maper() {
   local FILENAME="$2"
   m_video=$(ffprobe $FILENAME 2>&1 | grep Video | awk '{print $2}' | sed -ne 1p | cut -c 2-4)
   if [[ "$?" -ne 0 ]]; then
-    job_set_failed $JOB_ID "err found video stream"
+    job_set_failed $JOB_ID "Failed to execute FFprobe (Video)"
     return 1
   elif [[ -z "$m_video" ]]; then
-    job_set_failed $JOB_ID "not found video stream"
+    job_set_failed $JOB_ID "Video stream not found"
     return 2
   fi
   m_audio_rus=$(ffprobe $FILENAME 2>&1 | grep rus | grep Audio | awk '{print $2}' | cut -c 2-4)
   if [[ "$?" -ne 0 ]]; then
-    job_set_failed $JOB_ID "err found rus audio stream"
+    job_set_failed $JOB_ID "Failed to execute FFprobe (Audio/Rus)"
     return 1
   elif [[ -z "$m_audio_rus" ]]; then
-    job_set_failed $JOB_ID "not found rus audio stream"
+    job_log $JOB_ID "Russian audio stream not found"
     m_audio_any=$(ffprobe $FILENAME 2>&1 | grep Audio | awk '{print $2}' | sed -ne 1p | cut -c 2-4)
     if [[ "$?" -ne 0 ]]; then
-      job_set_failed $JOB_ID "err found any audio stream"
+      job_set_failed $JOB_ID "Failed to execute FFprobe (Audio)"
       return 1
     elif [[ -z "$m_audio_eng" ]]; then
-      job_set_failed $JOB_ID "not found any audio stream"
+      job_set_failed $JOB_ID "Audio stream not found"
       return 2
     fi
     m_audio=$($m_audio_any)
