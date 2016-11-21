4e516c7c822c76f872cbe35c8e2a23b9702110f3
jidckii <jidckii@gmail.com>
2016-11-21 22:23:04 +0500

    fix maper

--- maper.sh
+++ maper.sh
@@ -17,7 +17,7 @@
 #######################################
 maper(){
   while true ;do
-    m_video="${ffprobe $file_name 2>&1 | grep Video | awk '{print $2}' | sed -ne 1p | cut -c 2-4}"
+    m_video=$(ffprobe $file_name 2>&1 | grep Video | awk '{print $2}' | sed -ne 1p | cut -c 2-4)
       if [[ "$?" -ne 0 ]]; then
         job_set_failed $JOB_ID "err found video stream"
         return 1
@@ -26,14 +26,14 @@ maper(){
         return 2
       fi
 
-    m_audio_rus="${ffprobe $file_name 2>&1 | grep rus | grep Audio | awk '{print $2}' | cut -c 2-4 }"
+    m_audio_rus=$(ffprobe $file_name 2>&1 | grep rus | grep Audio | awk '{print $2}' | cut -c 2-4)
       if [[ "$?" -ne 0 ]]; then
         job_set_failed $JOB_ID "err found rus audio stream"
         return 1
       elif [[ -z "$m_audio_rus" ]]; then
         job_set_failed $JOB_ID "not found rus audio stream"
 
-        m_audio_any="${ffprobe $file_name 2>&1 | grep Audio | awk '{print $2}' | sed -ne 1p | cut -c 2-4 }"
+        m_audio_any=$(ffprobe $file_name 2>&1 | grep Audio | awk '{print $2}' | sed -ne 1p | cut -c 2-4)
         if [[ "$?" -ne 0 ]]; then
           job_set_failed $JOB_ID "err found any audio stream"
           return 1
@@ -41,10 +41,10 @@ maper(){
           job_set_failed $JOB_ID "not found any audio stream"
           return 2
         fi
-        m_audio="${$m_audio_any}"
+        m_audio=$($m_audio_any)
         break
       fi
-    m_audio="${$m_audio_rus}"
+    m_audio=$($m_audio_rus)
     break
   done
 }
