62b6a5de36afee37a6ad06899bbab1a473795e2e
leoderoko <leoderoko@gmail.com>
2016-12-22 17:52:25 +0500

    Fix codestyle

--- watcher.sh
+++ watcher.sh
@@ -16,12 +16,12 @@ watcher(){
   local JOB_ID="$1"
   local PID="$2"
   while kill -0 $PID; do
-  	STATE=$(job_get_state $JOB_ID)
-  	if [ "$STATE" == "canceled" ]; then
-  		kill -9 $PID
-  		job_log $JOB_ID "Administrative canceled !"
-  		return 1
-  	fi
-  	sleep 20
+    STATE=$(job_get_state $JOB_ID)
+    if [ "$STATE" == "canceled" ]; then
+      kill -9 $PID
+      job_log $JOB_ID "Administrative canceled !"
+      return 1
+    fi
+    sleep 20
   done
 }
